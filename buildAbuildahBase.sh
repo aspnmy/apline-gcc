3a364705e090fe880668b25be37dc14e8d7a19bc
aspnmy <support@e2bank.cn>
2025-09-27 12:41:52 +0800

    feat: 添加基于alpine的buildah和qemu-user-static交叉编译镜像
    添加一个最小化的Docker镜像，包含buildah和qemu-user-static用于交叉编译环境，同时集成常用工具如bash、curl、git等

---
+++ buildAbuildahBase.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+URI="docker.io"
+GithubURI="ghcr.io"
+Gitte="git.t2be.cn"
+AUUSER="aspnmy"
+imgNAME="apline-buildah-qemu-user-static"
+s6OverlayVer="3210"
+packageVer="3221"
+timeBuild=$(date +"%Y%m%d%H")
+#ver="${timeBuild}_s6_overlay_v${s6OverlayVer}_${debianVer}_BestHostsMonitor"
+stableVer="${timeBuild}-${imgNAME}-${packageVer}-base"
+
+# 构建镜像
+buildah bud --no-cache -f ./Dockerfile-apline-gcc-base -t $URI/$AUUSER/$imgNAME:$stableVer
+if [ $? -ne 0 ]; then
+    echo "构建镜像失败，退出脚本"
+    exit 1
+fi
+
+# 打标签
+buildah tag $URI/$AUUSER/$imgNAME:$stableVer $GithubURI/$AUUSER/$imgNAME:$stableVer
+buildah tag $URI/$AUUSER/$imgNAME:$stableVer $Gitte/$AUUSER/$imgNAME:$stableVer
+
+# 推送镜像逻辑
+if buildah push $URI/$AUUSER/$imgNAME:$stableVer; then
+    echo "Docker Hub推送成功"
+    if buildah push $GithubURI/$AUUSER/$imgNAME:$stableVer; then
+        echo "GitHub Container Registry推送成功"
+    else
+        echo "GitHub Container Registry推送失败"
+    fi
+elif buildah push $GithubURI/$AUUSER/$imgNAME:$stableVer; then
+    echo "Docker Hub推送失败，但GitHub Container Registry推送成功"
+    if buildah push $Gitte/$AUUSER/$imgNAME:$stableVer; then
+        echo "Gitte Registry推送成功"
+    else
+        echo "Gitte Registry推送失败"
+    fi
+else
+    echo "所有镜像仓库推送失败"
+    exit 1
+fi
